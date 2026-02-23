18cc6daa65ca627273918fac31a78e2e3f4aaec6
Josue Marquez <owaruuu@gmail.com>
2026-02-23 10:46:54 -0300

    add 0.0.0.0 ip

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,6 @@ app.get("/", (req, res) => {
     res.send("Hello from second server.");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server listening on port ${PORT}`);
 });
